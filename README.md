1cc8a41c58db7b754fc12e07ecc929ee79c7a1e4
alexandravg <36479153+alexandravg@users.noreply.github.com>
2019-05-23 17:36:40 +0700

    First server implementation

--- README.md
+++ README.md
@@ -7,5 +7,9 @@ mvn -DskipTests package
 
 Run server:
 ```bash
-mvn -DskipTests package exec:java -Dexec.mainClass=com.example.grpc.App
+mvn -DskipTests package exec:java -Dexec.mainClass=ru.megafon.deer_messenger_backend.App
+```
+
+```bash
+mvn -DskipTests package exec:java -Dexec.mainClass=ru.megafon.deer_messenger_backend.Client
 ```
--- src/main/java/ru/megafon/deer_messenger_backend/MessengerImpl.java
+++ src/main/java/ru/megafon/deer_messenger_backend/MessengerImpl.java
@@ -2,24 +2,42 @@ package ru.megafon.deer_messenger_backend;
 
 import io.grpc.stub.StreamObserver;
 
+import java.util.HashSet;
+
 public class MessengerImpl extends MessengerGrpc.MessengerImplBase {
+    public Integer idMessege = 0;
+    public HashSet<DeerMessengerBackend.Message> dataBase = new HashSet<DeerMessengerBackend.Message>();
+
+
     @Override
     public void sendMessage(DeerMessengerBackend.SendMessageRequest request,
-                         StreamObserver<DeerMessengerBackend.SendMessageResponse> responseObserver) {
+                         StreamObserver<DeerMessengerBackend.SendMessageResponse> responseObserver)  {
+        if(request.hasMessage()) {
+            idMessege++;
 
-        System.out.println(request);
+            //System.out.println(request);
 
-        DeerMessengerBackend.Message message = DeerMessengerBackend.Message.newBuilder()
-                .setText("Hello there, " + request.getMessage().getText())
-                .build();
+            DeerMessengerBackend.Message message = DeerMessengerBackend.Message.newBuilder()
+                    .setCreatedAt(System.currentTimeMillis())
+                    .setId(idMessege)
+                    .setSender(request.getMessage().getSender())
+                    .setText(request.getMessage().getText())
+                    .setReceiver(request.getMessage().getReceiver())
+                    .build();
 
-        DeerMessengerBackend.SendMessageResponse response = DeerMessengerBackend.SendMessageResponse.newBuilder()
-                .setMessage(message)
-                .build();
+            DeerMessengerBackend.SendMessageResponse response = DeerMessengerBackend.SendMessageResponse.newBuilder()
+                    .setMessage(message)
+                    .build();
 
-        responseObserver.onNext(response);
+            //System.out.println(response);
+            dataBase.add(message);
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+        else {
+            System.out.println("Error");
 
-        responseObserver.onCompleted();
+        }
     }
 
     @Override
@@ -27,8 +45,28 @@ public class MessengerImpl extends MessengerGrpc.MessengerImplBase {
                             StreamObserver<DeerMessengerBackend.GetMessagesStreamResponse> responseObserver) {
 
         System.out.println(request);
+        int idSender = request.getMessage().getSender().getId();
+        int idReciever = request.getMessage().getReceiver().getId();
+        HashSet<DeerMessengerBackend.Message> subBase = new HashSet<DeerMessengerBackend.Message>();
 
-        for(int i=0; i<3; i++){
+        for (DeerMessengerBackend.Message mes : dataBase){
+            if ((mes.getSender().getId()==idSender)&&(mes.getReceiver().getId()==idReciever))
+                subBase.add(mes);
+            if ((mes.getSender().getId()==idReciever)&&(mes.getReceiver().getId()==idSender))
+                subBase.add(mes);
+        }
+
+
+        for(DeerMessengerBackend.Message mes : subBase){
+
+            DeerMessengerBackend.GetMessagesStreamResponse response = DeerMessengerBackend.GetMessagesStreamResponse.newBuilder()
+                    .addMessage(mes)
+                    .build();
+
+            responseObserver.onNext(response);
+        }
+
+       /* for(int i=0; i<3; i++){
             DeerMessengerBackend.Message message = DeerMessengerBackend.Message.newBuilder()
                 .setText("Lol, wow #" + i)
                 .build();
@@ -38,7 +76,7 @@ public class MessengerImpl extends MessengerGrpc.MessengerImplBase {
                     .build();
 
             responseObserver.onNext(response);
-        }
+        }*/
 
         responseObserver.onCompleted();
     }
