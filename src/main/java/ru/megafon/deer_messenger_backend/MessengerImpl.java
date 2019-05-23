92357c6ced9c61f3f78a7271b524a4d0061c1bbc
alexandravg <36479153+alexandravg@users.noreply.github.com>
2019-05-23 18:21:19 +0700

    Second server implementation

--- src/main/java/ru/megafon/deer_messenger_backend/MessengerImpl.java
+++ src/main/java/ru/megafon/deer_messenger_backend/MessengerImpl.java
@@ -7,7 +7,54 @@ import java.util.HashSet;
 public class MessengerImpl extends MessengerGrpc.MessengerImplBase {
     public Integer idMessege = 0;
     public HashSet<DeerMessengerBackend.Message> dataBase = new HashSet<DeerMessengerBackend.Message>();
+    public HashSet<DeerMessengerBackend.Deer> deerBase = new HashSet<DeerMessengerBackend.Deer>();
+    public Integer idDeer = 0;
 
+    @Override
+    public void loginDeer(DeerMessengerBackend.LoginDeerRequest request,
+                          io.grpc.stub.StreamObserver<DeerMessengerBackend.LoginDeerResponse> responseObserver){
+        String tempdeer = request.getDeer().getName();
+        boolean isfound = false;
+        for (DeerMessengerBackend.Deer der : deerBase){
+            if(der.getName().equalsIgnoreCase(tempdeer)){
+                isfound = true;
+                DeerMessengerBackend.LoginDeerResponse response = DeerMessengerBackend.LoginDeerResponse.newBuilder()
+                        .setDeer(der)
+                        .build();
+
+                responseObserver.onNext(response);
+            }
+        }
+        if(!isfound){
+            idDeer++;
+
+            DeerMessengerBackend.Deer der = DeerMessengerBackend.Deer.newBuilder()
+                    .setId(idDeer)
+                    .setName(tempdeer)
+                    .build();
+            deerBase.add(der);
+            DeerMessengerBackend.LoginDeerResponse response = DeerMessengerBackend.LoginDeerResponse.newBuilder()
+                    .setDeer(der)
+                    .build();
+
+            responseObserver.onNext(response);
+        }
+    }
+
+    @Override
+    public void getDeersStream(DeerMessengerBackend.GetDeersStreamRequest request,
+                               StreamObserver<DeerMessengerBackend.GetDeersStreamResponse> responseObserver){
+        for(DeerMessengerBackend.Deer der : deerBase){
+
+            DeerMessengerBackend.GetDeersStreamResponse response = DeerMessengerBackend.GetDeersStreamResponse.newBuilder()
+                    .addDeer(der)
+                    .build();
+
+            responseObserver.onNext(response);
+        }
+        responseObserver.onCompleted();
+
+    }
 
     @Override
     public void sendMessage(DeerMessengerBackend.SendMessageRequest request,
@@ -65,19 +112,6 @@ public class MessengerImpl extends MessengerGrpc.MessengerImplBase {
 
             responseObserver.onNext(response);
         }
-
-       /* for(int i=0; i<3; i++){
-            DeerMessengerBackend.Message message = DeerMessengerBackend.Message.newBuilder()
-                .setText("Lol, wow #" + i)
-                .build();
-
-            DeerMessengerBackend.GetMessagesStreamResponse response = DeerMessengerBackend.GetMessagesStreamResponse.newBuilder()
-                    .addMessage(message)
-                    .build();
-
-            responseObserver.onNext(response);
-        }*/
-
         responseObserver.onCompleted();
     }
 }
